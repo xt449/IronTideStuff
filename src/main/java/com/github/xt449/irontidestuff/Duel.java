8b7ac9921e3737bae26cf5e76ec6485ee3169af5
xt449 <alpha.xt449.omega@gmail.com>
2020-10-13 13:21:42 -0400

    First step on adding duel mechanic

---
+++ src/main/java/com/github/xt449/irontidestuff/Duel.java
@@ -0,0 +1,22 @@
+package com.github.xt449.irontidestuff;
+
+import java.util.HashSet;
+import java.util.UUID;
+
+/**
+ * @author xt449 / BinaryBanana
+ */
+class Duel {
+
+	final HashSet<UUID> particpants = new HashSet<>();
+
+	boolean started = false;
+
+	/*void addParticipant(Player player) {
+		particpants.add(player.getUniqueId());
+	}
+
+	void removeParticipant(Player player) {
+		particpants.remove(player.getUniqueId());
+	}*/
+}
--- src/main/java/com/github/xt449/irontidestuff/IronTideStuff.java
+++ src/main/java/com/github/xt449/irontidestuff/IronTideStuff.java
@@ -3,20 +3,35 @@ package com.github.xt449.irontidestuff;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.projectiles.ProjectileSource;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * @author xt449 / BinaryBanana
+ */
 public final class IronTideStuff extends JavaPlugin implements Listener {
 
     private static final String OVERWORLD_PREFIX = ChatColor.GREEN + "Overworld " + ChatColor.WHITE;
     private static final String NETHER_PREFIX = ChatColor.RED + "Nether " + ChatColor.WHITE;
     private static final String END_PREFIX = ChatColor.YELLOW + "End " + ChatColor.WHITE;
     private static final String OTHER_PREFIX = ChatColor.GRAY + "Other " + ChatColor.WHITE;
+
+    private final HashMap<UUID, Duel> duelMap = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -47,6 +62,11 @@ public final class IronTideStuff extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+
     @EventHandler(priority = EventPriority.MONITOR)
     private void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
         updatePlayerListName(event.getPlayer(), event.getPlayer().getWorld().getEnvironment());
@@ -55,6 +75,10 @@ public final class IronTideStuff extends JavaPlugin implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     private void onPlayerJoin(PlayerJoinEvent event) {
         updatePlayerListName(event.getPlayer(), event.getPlayer().getWorld().getEnvironment());
+
+        // TODO
+        //duelMap.put(event.getPlayer().getUniqueId(), new Duel());
+        //duelMap.values().forEach(duel -> duel.particpants.addAll(Bukkit.getOnlinePlayers().stream().map(Entity::getUniqueId).collect(Collectors.toSet())));
     }
 
     private void updatePlayerListName(Player player, World.Environment environment) {
@@ -73,6 +97,42 @@ public final class IronTideStuff extends JavaPlugin implements Listener {
             }
             default: {
                 player.setPlayerListName(OTHER_PREFIX + player.getName());
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    private void onPlayerDamage(EntityDamageByEntityEvent event) {
+        if(event.getEntity() instanceof Player) {
+            final Player target = (Player) event.getEntity();
+            final Duel duel = duelMap.get(target.getUniqueId());
+
+            final Player damager;
+
+            if(event.getDamager() instanceof Player) {
+                damager = (Player) event.getDamager();
+            } else if(event.getDamager() instanceof Projectile) {
+                final ProjectileSource source = ((Projectile) event.getDamager()).getShooter();
+
+                if(source instanceof Player) {
+                    damager = (Player) source;
+                } else {
+                    return;
+                }
+            } else if(event.getDamager() instanceof TNTPrimed) {
+                final Entity source = ((TNTPrimed) event.getDamager()).getSource();
+
+                if(source instanceof Player) {
+                    damager = (Player) source;
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+
+            if(duel == null || !duel.particpants.contains(damager.getUniqueId())) {
+                event.setCancelled(true);
             }
         }
     }
